b2dc623770f24955a88a00f6588beaf78b92ba60
AliReaza <alireazamagh@gmail.com>
2022-12-10 13:16:31 +0330

    PHP Form Data

---
+++ tests/Unit/FormDataTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AliReaza\Tests\Component\HttpFoundation\Request;
+
+use PHPUnit\Framework\TestCase;
+
+class FormDataTest extends TestCase
+{
+    public function test_Coming_Soon()
+    {
+        $this->markTestIncomplete('This test has not been implemented yet.');
+    }
+}
